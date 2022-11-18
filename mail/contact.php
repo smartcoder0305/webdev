9553b44080400f44b8482519a529e636d961d88d
smartcoder0305 <stunningcoder7@gmail.com>
2022-11-18 10:15:32 -0800

    update

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "mianhua9999@gmail.com"; // Change this email to your //
+$to = "akihiko.star.7@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
